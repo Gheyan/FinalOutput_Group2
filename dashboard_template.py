20a79373f9d436cc1f6d2a1e8ff9298d567356e5
AHelbes <ahbalagao@gmail.com>
2024-11-15 11:39:21 +0800

    Update dashboard_template.py

--- dashboard_template.py
+++ dashboard_template.py
@@ -570,7 +570,51 @@ elif st.session_state.page_selection == "data_cleaning":
     st.subheader('y (dependent variable)')
     st.dataframe(y_Linear, use_container_width=True,hide_index=True)
 
+    # BALAGAO - Data Cleaning for Linear Regression Model
+    st.subheader('Actual vs Predicted Global Sales (Linear Regression Model)')
+    LinearRegData = {
+        'NA_Sales': dataset['NA_Sales'],
+        'EU_Sales': dataset['EU_Sales'],
+        'JP_Sales': dataset['JP_Sales'],
+        'Other_Sales': dataset['Other_Sales'],
+        'Global_Sales': dataset['Global_Sales']
+    }
+    # Create a DataFrame from the dictionary
+    df = pd.DataFrame(LinearRegData)
+    st.dataframe(df_data_Linear, use_container_width=True,hide_index=True)
+    st.markdown('...')
 
+    # Check for null values in 'Genre' and 'Global_Sales' columns
+    null_counts = df_data_Linear[['Genre', 'Global_Sales']].isnull().sum()
+    if null_counts.any():
+        st.warning(f"Null values found:\n{null_counts[null_counts > 0]}")
+    else:
+        st.success("No null values found in the selected columns.")
+
+    st.markdown('Using the `.isnull().sum()` in our specific chunk of the dataset, we will check if there are any null values that need to be processed. Since no null values are found, no extra processing in terms of removing or reprocessing the missing values is required.')
+
+    # X - independent and y - dependent
+    st.subheader('Splitting the Data into X (independent variable) and y (dependent variable)')
+    st.code("""
+        regions = ['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']
+
+        plt.figure(figsize=(12, 10))
+        for i, region in enumerate(regions):
+            X = sales_data[[region]]  # Independent variable (region)
+        y = sales_data['Global_Sales']  # Dependent variable (Global_Sales)
+        """)
+
+    # Define the var
+    X_Linear = df_data_Linear[['NA_Sales', 'EU_Sales', 'JP_Sales', 'Other_Sales']]  # Independent variables (regional sales)
+    y_Linear = df_data_Linear['Global_Sales']  # Dependent variable (Global_Sales)
+
+    # Independent var - display the dataframe
+    st.subheader('X (independent variable)')
+    st.dataframe(X_Linear, use_container_width=True, hide_index=True)
+
+    # Dependent var - display the series
+    st.subheader('y (dependent variable)')
+    st.dataframe(y_Linear, use_container_width=True, hide_index=True)
 
 
 # Machine Learning Page
